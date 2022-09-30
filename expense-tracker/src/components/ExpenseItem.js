f000963d8e8094d75758144303f53ef6f868c0de
codewithrabeeh <rabeehebrahim48@gmail.com>
2022-09-30 13:59:44 +0530

    concept of composition

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2021, 2, 28)
-    // const expenseTitle = 'Car Insurance'
-    // const expenseAmount = '$443'
   return (
-    <div className='expense-item'>
-        <div>{props.date}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
-        </div>
-    </div>
-  )
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
