4531804a4015ef69ede4b96c88c19bd9cea2adc0
codewithrabeeh <rabeehebrahim48@gmail.com>
2022-10-26 10:02:20 +0530

    small changes to custom-react-hook

--- custom-react-hook/src/hooks/use-counter.js
+++ custom-react-hook/src/hooks/use-counter.js
@@ -3,13 +3,13 @@ import { useState, useEffect } from "react";
 const useCounter = (num) => {
   const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
+  useEffect(() => { 
     const interval = setInterval(() => {
       setCounter((prevCounter) => prevCounter + num);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [num]);
 
   return counter
 };
