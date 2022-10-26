2e4e45be52f036125d6602e690b9f5294e7ab26d
codewithrabeeh <rabeehebrahim48@gmail.com>
2022-10-26 09:10:10 +0530

    finished http-request

--- http-request/src/App.js
+++ http-request/src/App.js
@@ -14,23 +14,26 @@ function App() {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://fir-7f478-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
 
       const data = await response.json();
+      
+      const loadedMovies = []
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setData(transformedMovies);
+      for(const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+
+      setData(loadedMovies);
       // setLoading(false);
     } catch (error) {
       setError(error.message);
@@ -43,8 +46,16 @@ function App() {
     movieBtnHandler()
   }, [movieBtnHandler])
 
-  function addMovieHandler(movie) {
-    console.log(movie)
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://fir-7f478-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
   }
 
   let content = <p>Found no movies.</p>
@@ -74,5 +85,3 @@ function App() {
 }
 
 export default App;
-
-/* 1:00 */
